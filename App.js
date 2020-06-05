ad724cc86504dd6a817b2651dfecc66df1064aad
10mi8o <totoma855@gmail.com>
2020-06-05 23:54:50 +0900

    navのルーティングを別ファイルに切り出し

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./screens/Home";
-
-const Stack = createStackNavigator();
+import AppNavigator from "./navigation/AppNavigator";
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Neeews!!!!" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigator />;
 };
 
 export default App;
