02452905f0de02fbf2c49f98af21b7da9a4034cd
10mi8o <totoma855@gmail.com>
2020-06-06 07:12:49 +0900

    Providerをimport

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import AppNavigator from "./navigation/AppNavigator";
-
+import { Provider } from "react-redux";
+import store from "./store";
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 };
 
 export default App;
